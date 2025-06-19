4bd95aa4ca39dbdfed44058efa426936bb662a7d
Flame256 <256dekrfy256@mail.ru>
2025-06-19 09:47:29 +0300

    main.py

--- main.py
+++ main.py
@@ -1,19 +1,24 @@
 import os
 from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+from telegram.ext import Application, CommandHandler, ContextTypes
+from apscheduler.schedulers.background import BackgroundScheduler
+from datetime import datetime
 
-# Получаем токен из переменной окружения
 TOKEN = os.getenv("TOKEN")
 
-# Пример простой команды /start
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Привет! Я твой PocketHelper 🤖")
 
-# Создаём и запускаем бота
-if __name__ == '__main__':
-    app = ApplicationBuilder().token(TOKEN).build()
+def scheduled_job():
+    print(f"[{datetime.now()}] Запланированная задача выполнена")
 
+def main():
+    app = Application.builder().token(TOKEN).build()
     app.add_handler(CommandHandler("start", start))
 
-    print("Бот запущен ✅")
-    app.run_polling()
+    scheduler = BackgroundScheduler()
+    scheduler.add_job(scheduled_job, "interval", minutes=1)
+    scheduler.start()
+
+    app.r
+
